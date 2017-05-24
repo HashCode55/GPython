275c9951f4fb2c2901414917bdca67cc91c6c78d
hashcode55 <blackdragon0909@gmail.com>
2017-05-24 20:57:20 +0530

    tests added

--- parser_test.go
+++ parser_test.go
@@ -1,1 +1,51 @@
 package gython
+
+import "testing"
+
+func TestParseSuccess(t *testing.T) {
+	_, err := ParseEngine("hello = 123 - 22", false)
+	if err != nil {
+		t.Error("Parsing failed for fine syntax.")
+	}
+}
+
+func TestParserFail(t *testing.T) {
+	_, err := ParseEngine("hello = 123 - aas", false)
+	if err == nil {
+		t.Error("Parser not failing when bad syntax.")
+	}
+
+	_, err = ParseEngine("ll = 12 + 12 + \"asd\"", false)
+	if err == nil {
+		t.Error("Parser not failing when bad syntax.")
+	}
+}
+
+func TestParseString(t *testing.T) {
+	_, err := ParseEngine("hello = \"hashcode\"", false)
+	if err != nil {
+		t.Error("Couldn't parse string allocation.")
+	}
+}
+
+func TestAST(t *testing.T) {
+	astNode, _ := ParseEngine("hello = 123 - 22", false)
+	tokenList := astNode.Traverse(astNode)
+	// Hard matching the AST tokens
+	hardTokens := []string{"=", "hello", "-", "123", "22"}
+	for i, token := range tokenList {
+		if token != hardTokens[i] {
+			t.Error("Error in constructing the abstract syntax tree")
+		}
+	}
+
+	astNode, _ = ParseEngine("hello = \"hashcode\"", false)
+	tokenList = astNode.Traverse(astNode)
+	// Hard matching the AST tokens
+	hardTokens = []string{"=", "hello", "\"hashcode\""}
+	for i, token := range tokenList {
+		if token != hardTokens[i] {
+			t.Error("Error in constructing the abstract syntax tree")
+		}
+	}
+}
--- tokenizer_test.go
+++ tokenizer_test.go
@@ -1,1 +1,69 @@
 package gython
+
+import (
+	"testing"
+)
+
+func TestTokensLength(t *testing.T) {
+	// Test the number of tokens lexed
+	// should be equal to 5 in this case
+	tokens := LexEngineTest("2 * 2 * 2")
+	if len(tokens) != 5 {
+		t.Errorf("5 tokens were expected, got %v", len(tokens))
+	}
+	tokens = LexEngineTest("hello = nothello")
+	if len(tokens) != 3 {
+		t.Errorf("3 tokens were expected, got %v", len(tokens))
+	}
+	tokens = LexEngineTest("hello = \"hello\"")
+	if len(tokens) != 3 {
+		t.Errorf("3 tokens were expected, got %v", len(tokens))
+	}
+}
+
+func TestTokenizerFail(t *testing.T) {
+	// Test if the lexer is failing at invalid input or not
+	// '?' is invalid here
+	tokens := LexEngineTest("< ? 2 * 2")
+	flag := false
+	for i := 0; i < len(tokens); i++ {
+		if tokens[i].Type_ == 0 {
+			flag = true
+		}
+	}
+	if !flag {
+		t.Errorf("Lexer not crashing at invalid input")
+	}
+}
+
+func TestLGTokens(t *testing.T) {
+	// Test for comparison operators
+	// 6 operators
+	tokens := LexEngineTest("<>>><<<=>=")
+	if len(tokens) != 6 {
+		t.Errorf("6 tokens were expected, got %v", len(tokens))
+	}
+}
+
+func TestStringFail(t *testing.T) {
+	tokens := LexEngineTest("hello = \"sajdhb")
+	flag := false
+	for i := 0; i < len(tokens); i++ {
+		if tokens[i].Type_ == 0 {
+			flag = true
+		}
+	}
+	if !flag {
+		t.Errorf("Lexer not crashing at invalid input")
+	}
+	tokens = LexEngineTest("hello = \"sajdhb' ")
+	flag = false
+	for i := 0; i < len(tokens); i++ {
+		if tokens[i].Type_ == 0 {
+			flag = true
+		}
+	}
+	if !flag {
+		t.Errorf("Lexer not crashing at invalid input")
+	}
+}
