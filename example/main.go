a6ac46c06001350f3c795d4ff425096a9d671572
hashcode55 <blackdragon0909@gmail.com>
2017-11-15 00:21:20 +0530

    parenthesis support

--- example/main.go
+++ example/main.go
@@ -8,11 +8,11 @@ package main
 // Remove local imports
 import (
 	"flag"
-	"github.com/hashcode55/Gpython"
+	"github.com/HashCode55/GPython"
 )
 
 func main() {
 	boolPtr := flag.Bool("log", true, "Set it to true to log the details.")
 	flag.Parse()
-	gpython.ParseEngine("a = ab", *boolPtr)
+	gpython.ParseEngine("hello = ( 3 * 6 ) - 5 / 2", *boolPtr)
 }
